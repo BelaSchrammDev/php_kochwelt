a1114e9929e70ad53a1caaefeba35a01d8315e20
BelaSchrammDev <belaschramm@aol.de>
2024-12-11 20:13:18 +0100

    add maincontent

--- app/controllers/main.php
+++ app/controllers/main.php
@@ -4,6 +4,7 @@ class MainController
     public function content()
     {
         $content_view = 'app/views/mainpage.php';
+        $content_css = ['./css/main.css'];
         include 'app/views/layout.php';
     }
 }
--- app/views/mainpage.php
+++ app/views/mainpage.php
@@ -1,3 +1,68 @@
-<div>
-    <h1>Main Content</h1>
-</div>
+<section class="main_container main_section">
+    <section class="main_container main_section section-recipe-of-the-day width_less_550px">
+        <img src="./img/salad.jpg" alt="">
+        <div class="croissant-description ">
+            <div>
+                <h2>Griechischer Bauernsalat</h2>
+                <span>
+                    Ein leckerer Salat, der schnell zubereitet und auch noch sehr gesund ist.
+                </span>
+            </div>
+            <button onclick="location.href='salad.html';" class="button-green">Rezept öffnen</button>
+        </div>
+    </section>
+
+    <section class="main_container main_section section-other-recipe">
+        <h2>Lust auf was neues?</h2>
+        <div class="other-recipes width_less_550px">
+            <div class="recipe">
+                <a class="recipe" href="./prasselkuchen.html">
+                    <img src="./img/prasselkuchen.jpg">
+                </a>
+                <span>Irenas Prasselkuchen</span>
+            </div>
+            <div class="recipe">
+                <a class="recipe" href="./baklava.html">
+                    <img src="./img/baklava.jpg" alt="">
+                </a>
+                <span>Türkische Baklava</span>
+            </div>
+            <div class="recipe">
+                <a class="recipe" href="./nudeln.html">
+                    <img src="./img/pasta.jpg" alt="">
+                </a>
+                <span>Nudeln mit scharfer<br>Tomatensoße</span>
+            </div>
+        </div>
+    </section>
+
+    <section class="main_container main_section section-wok-vs-pan">
+        <h2>Pfanne vs. Wok - Was ist besser?</h2>
+        <div class="wok-vs-pan-description width_less_550px">
+            <span>
+                Der offensichtlichste und wichtigste Unterschied ist die Form. Ein Wok ist kugelig und es gibt
+                keinen
+                Unterschied zwischen dem Boden und der Wand. Der Boden einer Pfanne hingegen ist flach. Die Größe
+                der
+                Fläche des Bodens kann dabei variieren. Hohe Pfannen werden auch als Schmorpfannen bezeichnet. Damit
+                ein
+                Wok auch in Haushaltsküchen Verwendung finden kann, haben diese zwar eine kugelige Form, sind am
+                Boden
+                aber auch minimal abgeflacht, damit sie auf dem Herd stehen können. <br>
+                <br>
+                Genau genommen ist ein Wok gleichzeitig auch eine Pfanne. Er unterscheidet sich jedoch von der
+                Form und Nutzung stark von herkömmlichen Bratpfannen, weswegen hier oftmals unterschieden wird. <br>
+                <br>
+                Eine Besonderheit beim Kochen mit einem Wok ist das Arbeiten mit sehr hohen Temperaturen. Während
+                die
+                optimale Temperatur im Wok bei 200 °C bis 250 °C liegt, liegt die ideale Temperatur für eine
+                Bratpfanne
+                deutlich darunter. In einer gewöhnlichen Pfanne sind Temperaturen von 140 °C bis 170 °C völlig
+                ausreichend.
+            </span>
+            <img src="./img/wok.jpg">
+        </div>
+
+    </section>
+
+</section>
